9056cff816a4d16e5cfed28c0129248c0c34f719
hexcity <howlettpete@gmail.com>
2019-04-01 16:10:38 -0300

    Updated loop with while forever

---
+++ peterwashere.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+@author: Peter Howlett
+
+@description: Across the screen
+'''
+
+import time
+
+str = "Peter Was Here"
+columns = 79
+sleeptime = 0.05
+
+while True:
+    for col in range(len(str),columns):
+        print(str.rjust(col))
+        time.sleep(sleeptime)
+
+    for coly in range(columns,len(str),-1):
+        print(str.rjust(coly))
+        time.sleep(sleeptime)
+
+exit()
