724efd849b8c8b8737749f33711273a71287fcf3
hexcity <howlettpete@gmail.com>
2021-06-16 12:20:42 -0300

    Java updates

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+/*****/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+
+        char a = 'a';
+        char b = 'b';
+        char c = 'c';
+
+        System.out.println("First" + a + b);
+
+        char n[] = new char[4];
+
+        n[0] = 'y';
+        n[1] = 'z';
+
+        System.out.println("First" + Arrays.toString(n));
+    }
+}
---
+++ echoserver.java
@@ -0,0 +1,25 @@
+/* A number of items */
+
+public class EchoServer
+{
+    public static void main(String args[])
+    {
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (
+            Socket echoSocket = new Socket(hostName, portNumber);
+            PrintWriter out =
+            new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+        )
+
+        String userInput;
+        while ((userInput = stdIn.readLine()) != null) {
+            out.println(userInput);
+            System.out.println("echo: " + in.readLine());
+        }
+    }
+}
