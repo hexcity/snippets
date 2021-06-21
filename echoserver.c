125a35d2e6fca22d05def904a7c27f5c3db176ec
hexcity <howlettpete@gmail.com>
2021-06-21 15:03:33 -0300

    added C

---
+++ echoserver.c
@@ -0,0 +1,16 @@
+/**********************
+ * Test echos
+ * 
+ * @author: @hexcity
+ * 
+ * Example echo server
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <socket.h>
+
+int main() {
+    printf("Hello World\n");
+    exit(0);
+}
