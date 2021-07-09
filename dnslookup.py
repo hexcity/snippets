a003fee618fa4bd02616195015522124181bda4d
hexcity <howlettpete@gmail.com>
2021-07-09 17:17:51 -0300

    dnslookup

---
+++ dnslookup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+####################################################################
+#
+
+import ipaddress
+import socket
+
+MyIP = ipaddress.ip_address('172.21.96.10')
+dName = "www.intel.com"
+dIP = "10.220.3.11"
+
+resName = socket.gethostbyname_ex(dName)
+try:
+    resNameAddr = socket.gethostbyaddr(dIP)
+except Exception as Ex:
+    resNameAddr = ['N/A']
+    print(f'No DNS found for {dIP}: Exception: {Ex}')
+
+print(f'IP: {MyIP} - RevIP is: {MyIP.reverse_pointer}')
+print(f'DNS name {dName}: {socket.gethostbyname(dName)}')
+print(f'gethostbyname_ex: {resName}')
+print(f'gethostbyaddr: {resNameAddr} - first entry: {resNameAddr[0]}')
